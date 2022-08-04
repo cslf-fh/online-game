32810e6ae6585e44cb40caa8ee39cc8544831e8c
cslf-fh <consadole.sapporo.love.football@gmail.com>
2022-08-05 01:35:50 +0900

    [Update]マッチングルームの型を追加

--- types.ts
+++ types.ts
@@ -5,3 +5,28 @@ export type PLAYER_INFO = {
   state: 'standby' | 'matching' | 'playing';
   roomId: string;
 };
+
+export type MATCHING_ROOM = {
+  makeRoomAt: number;
+  roomId: string;
+  player1: MATCHING_PLAYER_INFO;
+  player2: MATCHING_PLAYER_INFO;
+  turn: 'player1' | 'player2';
+  matchState: 'matching' | 'playing';
+  boardState: BOARD_STATE;
+};
+
+export type MATCHING_PLAYER_INFO = {
+  userId: string;
+  name: string;
+  state: 'connected' | 'disconnected';
+};
+
+export type TILE_STATE = {
+  state: -1 | 0 | 1;
+  move: number;
+};
+
+export type BOARD_STATE_ROW = [TILE_STATE, TILE_STATE, TILE_STATE];
+
+export type BOARD_STATE = [BOARD_STATE_ROW, BOARD_STATE_ROW, BOARD_STATE_ROW];
